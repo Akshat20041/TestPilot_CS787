ba43d7ed619853b70b0730b009ea05b07b325b9e
Akshat20041 <134600061+Akshat20041@users.noreply.github.com>
2025-11-03 22:53:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import subprocess
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 # ------------------------- Setup -------------------------
